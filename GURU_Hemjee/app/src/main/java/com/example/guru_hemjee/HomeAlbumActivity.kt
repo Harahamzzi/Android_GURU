6df86c043514390952d82e1de217704c1074e7f5
bee-p <weileenw@naver.com>
2022-01-20 05:32:15 +0900

    Apply: home album UI 일부 적용(수정/추가 필요)

---
+++ GURU_Hemjee/app/src/main/java/com/example/guru_hemjee/HomeAlbumActivity.kt
@@ -0,0 +1,11 @@
+package com.example.guru_hemjee
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HomeAlbumActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home_album)
+    }
+}
