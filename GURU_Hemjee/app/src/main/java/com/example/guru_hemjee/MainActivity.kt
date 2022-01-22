889a3f05a4af1ba3db939e4ce18e4b8fa67dc4c0
bee-p <weileenw@naver.com>
2022-01-22 21:48:12 +0900

    Fix: 햄버거 메뉴 일부 수정(미완)

--- GURU_Hemjee/app/src/main/java/com/example/guru_hemjee/MainActivity.kt
+++ GURU_Hemjee/app/src/main/java/com/example/guru_hemjee/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.example.guru_hemjee
 
 import android.os.Bundle
+import android.widget.Toolbar
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.FragmentActivity
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : FragmentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-    }
+
+    // TODO: Navigation drawer menu 동작 구현
+//    lateinit var navigationView: NavigationView
+//    lateinit var drawerLayout: DrawerLayout
+//
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity_main)
+//
+//        val toolbar: Toolbar = findViewById(R.id.toolBar)
+//        setActionBar(toolbar)
+//
+//
+//    }
 }
