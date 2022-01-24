5e9340387ce25d7afb26a7a51def7375b104c610
bee-p <weileenw@naver.com>
2022-01-24 11:12:35 +0900

    Apply: 드롭다운 버튼 적용(Spinner)
    홈 리포트 화면 내의 목표 분류 선택 버튼 Spinner로 구현
    
    spinner에 들어갈 배경은 spinner_select_background.xml에 제작
    아이템 리스트는 spinner_item_list.xml 참고
    
    각 버튼 별 동작은 추후 구현 예정

--- GURU_Hemjee/app/src/main/java/com/example/guru_hemjee/HomeReportFragment.kt
+++ GURU_Hemjee/app/src/main/java/com/example/guru_hemjee/HomeReportFragment.kt
@@ -5,6 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 
 
 class HomeReportFragment : Fragment() {
@@ -18,5 +21,37 @@ class HomeReportFragment : Fragment() {
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home_report, container, false)
+
+        // spinner 연결
+        val spinner: Spinner = requireView().findViewById(R.id.goalSpinner)
+
+        // spinner 어댑터 설정
+        spinner.adapter = ArrayAdapter.createFromResource(requireContext(), R.array.spinnerGoalList, android.R.layout.simple_spinner_item)
+
+        // spinner 아이템 선택 리스너
+        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+
+            }
+
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                when(position){
+                    // 대표 목표 선택
+                    0 -> {
+                        // 대표 목표 클릭했을 때의..동작
+                    }
+
+                    // 세부 목표 선택
+                    1 -> {
+                        // 세부 목표 클릭했을 때의 동작..
+                    }
+
+                    // 일치하는 게 없을 경우
+                    else -> {
+
+                    }
+                }
+            }
+        }
     }
 }
