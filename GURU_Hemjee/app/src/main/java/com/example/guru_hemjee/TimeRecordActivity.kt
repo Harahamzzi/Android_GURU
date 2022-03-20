10188537c8565c8c900209c4cd229158636e3c26
yooonji08 <yooonji08@naver.com>
2022-03-20 22:37:05 +0900

    Apply: 기록 화면 추가
    - xml 파일만 구성함

---
+++ GURU_Hemjee/app/src/main/java/com/example/guru_hemjee/TimeRecordActivity.kt
@@ -0,0 +1,12 @@
+package com.example.guru_hemjee
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class TimeRecordActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_time_record)
+    }
+}
