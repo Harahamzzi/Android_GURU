146764191d17c48174a96e93cba1c62577ce7161
yooonji08 <yooonji08@naver.com>
2022-06-28 03:12:20 +0900

    [Delete] 다른 구조의 리사이클러뷰 어댑터 삭제
    - 현재 리사이클러뷰 어댑터의 구조와 다르게 작성한 리사이클러뷰 어댑터 삭제

--- GURU_Hemjee/app/src/main/java/com/example/guru_hemjee/Home/Goal/BigGoalRecyclerViewItem.kt
+++
@@ -1,8 +0,0 @@
-package com.example.guru_hemjee.Home.Goal
-
-// 대표 목록 커스텀 리사이클러뷰에 들어갈 요소들
-data class BigGoalRecyclerViewItem(
-        val colorImg: Int,                   // 대표목표 색상
-        val bigGoalText: String,             // 대표 목표
-        val iconArray: ArrayList<Int>        // 세부목표 아이콘 리스트
-)
