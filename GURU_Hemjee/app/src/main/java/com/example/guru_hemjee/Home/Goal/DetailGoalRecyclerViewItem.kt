d3373df430062aff040f9e287c8a1fb308e29ef4
yooonji08 <yooonji08@naver.com>
2022-06-28 03:08:21 +0900

    [Delete] 불필요한 파일 삭제
    - 1.0.0v버전에 쓰던 파일 삭제

--- GURU_Hemjee/app/src/main/java/com/example/guru_hemjee/Home/Goal/DetailGoalRecyclerViewItem.kt
+++
@@ -1,7 +0,0 @@
-package com.example.guru_hemjee.Home.Goal
-
-// 세부목표 커스텀 리사이클러뷰에 들어갈 요소들
-data class DetailGoalRecyclerViewItem(
-    val IconImg: Int,                  // 세부목표 아이콘
-    val detailGoalText: String,        // 세부 목표
-)
