41deff1394e508fd2651a03768c72a08ce8f497b
bee-p <weileenw@naver.com>
2022-09-01 00:03:15 +0900

    [Update] 설정 페이지 구현

--- GURU_Hemjee/app/src/main/java/com/example/guru_hemjee/Home/Setting/SettingIntroduceFragment.kt
+++ GURU_Hemjee/app/src/main/java/com/example/guru_hemjee/Home/Setting/SettingIntroduceFragment.kt
@@ -5,10 +5,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.guru_hemjee.R
 
 class SettingIntroduceFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_setting_introduce, container, false)
     }
 }
--- GURU_Hemjee/app/src/main/java/com/example/guru_hemjee/Home/Setting/SettingNoticeFragment.kt
+++ GURU_Hemjee/app/src/main/java/com/example/guru_hemjee/Home/Setting/SettingNoticeFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.guru_hemjee.R
 
 class SettingNoticeFragment : Fragment() {
 
@@ -12,4 +13,9 @@ class SettingNoticeFragment : Fragment() {
         super.onCreate(savedInstanceState)
     }
 
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_setting_notice, container, false)
+    }
 }
