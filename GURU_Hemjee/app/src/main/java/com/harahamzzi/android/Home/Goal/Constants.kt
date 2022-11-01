07cdc8c6652fb6568b013676d1d13420d4bbba02
yooonji08 <yooonji08@naver.com>
2022-11-01 11:02:43 +0900

    [Delete] 불필요한 파일 삭제

--- GURU_Hemjee/app/src/main/java/com/harahamzzi/android/Home/Goal/Constants.kt
+++
@@ -1,6 +0,0 @@
-package com.harahamzzi.android.Home.Goal
-
-object Constants {
-    const val PARENT = 0
-    const val CHILD = 1
-}
--- GURU_Hemjee/app/src/main/java/com/harahamzzi/android/Home/Store/ReceiptItem.kt
+++ GURU_Hemjee/app/src/main/java/com/harahamzzi/android/Home/Store/ReceiptItem.kt
@@ -1,4 +1,3 @@
 package com.harahamzzi.android.Home.Store
 
-class ReceiptItem(val picSource: Int, val price: Int, val type: String) {
-}
+class ReceiptItem(val picSource: Int, val price: Int, val type: String)
