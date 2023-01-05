adacca33d87602c9081659a0fa68d2aa0b5d86f6
bee-p <weileenw@naver.com>
2023-01-06 00:39:31 +0900

    [Add] 라이센스 text view 동작 추가
    - 토글 버튼 애니메이션 적용
    - 토글 버튼에 따라 해당 라이센스가 보여지고/감춰지는 기능 추가

--- GURU_Hemjee/app/src/main/java/com/harahamzzi/android/Home/Setting/SettingOpenSourceFragment.kt
+++ GURU_Hemjee/app/src/main/java/com/harahamzzi/android/Home/Setting/SettingOpenSourceFragment.kt
@@ -1,14 +1,16 @@
 package com.harahamzzi.android.Home.Setting
 
+import android.animation.ObjectAnimator
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ToggleButton
 import com.harahamzzi.android.R
 import com.harahamzzi.android.databinding.FragmentSettingOpenSourceBinding
 
-class SettingOpenSourceFragment : Fragment() {
+class SettingOpenSourceFragment : Fragment(), View.OnClickListener {
 
     // 뷰 바인딩 변수
     private var mBinding: FragmentSettingOpenSourceBinding? = null
@@ -27,5 +29,65 @@ class SettingOpenSourceFragment : Fragment() {
         mBinding = null
 
         super.onDestroyView()
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        binding.toggleButton1.setOnClickListener(this)
+        binding.toggleButton2.setOnClickListener(this)
+        binding.toggleButton3.setOnClickListener(this)
+        binding.toggleButton4.setOnClickListener(this)
+        binding.toggleButton5.setOnClickListener(this)
+        binding.toggleButton6.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+
+        var toggleButton: ToggleButton = v as ToggleButton
+
+        when (toggleButton.id) {
+
+            R.id.toggleButton1, R.id.toggleButton2, R.id.toggleButton3, R.id.toggleButton4
+                , R.id.toggleButton5, R.id.toggleButton6 -> {
+
+                if (toggleButton.isChecked)
+                {
+                    ObjectAnimator.ofFloat(v, View.ROTATION, 0f, 90f).apply {
+                        duration = 300
+                        start()
+                    }
+
+                    // 라이센스 텍스트 박스 보이기
+                    when (toggleButton.id) {
+
+                        R.id.toggleButton1 -> binding.textView1.visibility = View.VISIBLE
+                        R.id.toggleButton2 -> binding.textView2.visibility = View.VISIBLE
+                        R.id.toggleButton3 -> binding.textView3.visibility = View.VISIBLE
+                        R.id.toggleButton4 -> binding.textView4.visibility = View.VISIBLE
+                        R.id.toggleButton5 -> binding.textView5.visibility = View.VISIBLE
+                        R.id.toggleButton6 -> binding.textView6.visibility = View.VISIBLE
+                    }
+                }
+                else
+                {
+                    ObjectAnimator.ofFloat(v, View.ROTATION, 90f, 0f).apply {
+                        duration = 300
+                        start()
+                    }
+
+                    // 라이센스 텍스트 박스 감추기
+                    when (toggleButton.id) {
+
+                        R.id.toggleButton1 -> binding.textView1.visibility = View.GONE
+                        R.id.toggleButton2 -> binding.textView2.visibility = View.GONE
+                        R.id.toggleButton3 -> binding.textView3.visibility = View.GONE
+                        R.id.toggleButton4 -> binding.textView4.visibility = View.GONE
+                        R.id.toggleButton5 -> binding.textView5.visibility = View.GONE
+                        R.id.toggleButton6 -> binding.textView6.visibility = View.GONE
+                    }
+                }
+            }
+        }
     }
 }
